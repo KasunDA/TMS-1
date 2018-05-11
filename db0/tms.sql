0887d3425e4f13dc7e943647a87b53019f790fbf
UKKPK <36922935+UKKPK@users.noreply.github.com>
2018-05-12 00:42:26 +0500

    DB 0

---
+++ db0/tms.sql
@@ -0,0 +1,741 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2018 at 09:28 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts_entry`
+--
+
+CREATE TABLE `accounts_entry` (
+  `ae_id` int(11) NOT NULL,
+  `datee` varchar(20) NOT NULL,
+  `bank_id` int(11) NOT NULL,
+  `action` varchar(10) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `amount` decimal(13,2) NOT NULL,
+  `check_number` varchar(30) DEFAULT NULL,
+  `previous_balance` decimal(13,2) NOT NULL,
+  `current_balance` decimal(13,2) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounts_entry`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agent`
+--
+
+CREATE TABLE `agent` (
+  `agent_id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `address` text NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `agent`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `bank_id` int(11) NOT NULL,
+  `short_form` varchar(60) NOT NULL,
+  `full_form` varchar(60) NOT NULL,
+  `account_title` varchar(60) NOT NULL,
+  `account_number` varchar(35) NOT NULL,
+  `address` text NOT NULL,
+  `balance` decimal(13,2) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bike`
+--
+
+CREATE TABLE `bike` (
+  `bike_id` int(11) NOT NULL,
+  `bike_number` varchar(30) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bike`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chart_of_account`
+--
+
+CREATE TABLE `chart_of_account` (
+  `coa_id` int(11) NOT NULL,
+  `short_form` varchar(16) NOT NULL,
+  `full_form` varchar(60) NOT NULL,
+  `address` text NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chart_of_account`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `cmp_id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `company`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consignee`
+--
+
+CREATE TABLE `consignee` (
+  `consignee_id` int(11) NOT NULL,
+  `short_form` varchar(100) NOT NULL,
+  `full_form` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `consignee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `container`
+--
+
+CREATE TABLE `container` (
+  `container_id` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `container`
+--
+
+INSERT INTO `container` (`container_id`, `type`, `status`) VALUES
+(1, 'Tanki', 1),
+(2, 'Dry Containers', 1),
+(3, 'Open Top Containers', 1),
+(4, 'Tunnel Container', 1),
+(5, 'Side Open Storage Container', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `container_entry`
+--
+
+CREATE TABLE `container_entry` (
+  `ce_id` int(11) NOT NULL,
+  `container_number` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `advance` int(11) NOT NULL,
+  `balance` int(11) NOT NULL,
+  `color` varchar(7) NOT NULL,
+  `mr_charges` int(11) NOT NULL,
+  `remarks` text,
+  `cm_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `container_entry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `container_movement`
+--
+
+CREATE TABLE `container_movement` (
+  `cm_id` int(11) NOT NULL,
+  `datee` varchar(11) NOT NULL,
+  `agent_id` int(11) NOT NULL,
+  `coa_id` int(11) NOT NULL,
+  `consignee_id` int(11) NOT NULL,
+  `movement` varchar(7) NOT NULL,
+  `empty_terminal_id` int(11) NOT NULL,
+  `from_yard_id` int(11) NOT NULL,
+  `to_yard_id` int(11) NOT NULL,
+  `container_size` tinyint(4) NOT NULL,
+  `party_charges` int(11) NOT NULL,
+  `lot_of` int(11) NOT NULL,
+  `line_id` int(11) NOT NULL,
+  `bl_cro_number` varchar(16) NOT NULL,
+  `job_number` varchar(16) NOT NULL,
+  `index_number` varchar(15) NOT NULL,
+  `rent` int(11) NOT NULL,
+  `container_id` int(11) NOT NULL,
+  `lolo_charges` int(11) NOT NULL,
+  `weight_charges` int(11) NOT NULL,
+  `paid_status` tinyint(4) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `container_movement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daily_description`
+--
+
+CREATE TABLE `daily_description` (
+  `dd_id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `daily_description`
+--
+
+INSERT INTO `daily_description` (`dd_id`, `name`, `status`) VALUES
+(1, 'lunch', 1),
+(2, 'Advance', 1),
+(3, 'Bike Expenses', 1),
+(4, 'Driver Salary', 1),
+(5, 'breakfast', 1),
+(6, 'Party Payment', 1),
+(7, 'Vehicle Monthly Payment', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diesel_entry`
+--
+
+CREATE TABLE `diesel_entry` (
+  `de_id` int(11) NOT NULL,
+  `datee` varchar(20) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `from_yard_id` int(11) NOT NULL,
+  `to_yard_id` int(11) NOT NULL,
+  `litre_rate` decimal(13,2) NOT NULL,
+  `litres` decimal(13,2) NOT NULL,
+  `extra_litres` decimal(13,2) NOT NULL,
+  `total` decimal(13,2) NOT NULL,
+  `description` text,
+  `status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `diesel_entry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diesel_limit`
+--
+
+CREATE TABLE `diesel_limit` (
+  `dl_id` int(11) NOT NULL,
+  `from_yard` int(11) NOT NULL COMMENT 'yard',
+  `to_yard` int(11) NOT NULL COMMENT 'yard',
+  `limit_litre` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `diesel_limit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exin`
+--
+
+CREATE TABLE `exin` (
+  `exin_id` int(11) NOT NULL,
+  `expense_id` int(11) DEFAULT NULL,
+  `income_id` int(11) DEFAULT NULL,
+  `datee` varchar(30) NOT NULL,
+  `previous_balance` bigint(20) NOT NULL,
+  `current_balance` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `expense_id` int(11) NOT NULL,
+  `datee` varchar(20) NOT NULL,
+  `dd_id` int(11) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `check_number` varchar(30) DEFAULT NULL,
+  `bank_id` int(11) DEFAULT NULL,
+  `amount` decimal(13,2) NOT NULL,
+  `vehicle_id` int(11) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `bike_id` int(11) DEFAULT NULL,
+  `paid_status` tinyint(4) NOT NULL DEFAULT '0',
+  `cmp_id` int(11) DEFAULT NULL,
+  `description` text,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `expenses`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `garage_entry`
+--
+
+CREATE TABLE `garage_entry` (
+  `ge_id` int(11) NOT NULL,
+  `datee` varchar(20) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `amount` decimal(13,2) NOT NULL,
+  `description` text,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `garage_entry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income`
+--
+
+CREATE TABLE `income` (
+  `income_id` int(11) NOT NULL,
+  `datee` varchar(20) NOT NULL,
+  `dd_id` int(11) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `check_number` varchar(30) DEFAULT NULL,
+  `bank_id` int(11) DEFAULT NULL,
+  `amount` decimal(13,2) NOT NULL,
+  `cmp_id` int(11) DEFAULT NULL,
+  `vehicle_id` int(11) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `description` text,
+  `status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `income`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `line`
+--
+
+CREATE TABLE `line` (
+  `line_id` int(11) NOT NULL,
+  `short_form` varchar(100) NOT NULL,
+  `full_form` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `line`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `login_id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`login_id`, `username`, `pass`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
+
+CREATE TABLE `vehicle` (
+  `vehicle_id` int(11) NOT NULL,
+  `owner_name` varchar(60) NOT NULL,
+  `driver_name` varchar(60) NOT NULL,
+  `vehicle_number` varchar(30) NOT NULL,
+  `engine_number` varchar(30) NOT NULL,
+  `chassis_number` varchar(30) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voucher`
+--
+
+CREATE TABLE `voucher` (
+  `voucher_number` int(11) NOT NULL,
+  `datee` varchar(30) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `check_number` varchar(30) DEFAULT NULL,
+  `bank_id` int(11) DEFAULT NULL,
+  `amount` bigint(20) NOT NULL,
+  `cm_id` int(11) DEFAULT NULL,
+  `vehicle_id` int(11) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `voucher`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `yard`
+--
+
+CREATE TABLE `yard` (
+  `yard_id` int(11) NOT NULL,
+  `short_form` varchar(100) NOT NULL,
+  `full_form` varchar(100) NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `location` varchar(60) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `yard`
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts_entry`
+--
+ALTER TABLE `accounts_entry`
+  ADD PRIMARY KEY (`ae_id`);
+
+--
+-- Indexes for table `agent`
+--
+ALTER TABLE `agent`
+  ADD PRIMARY KEY (`agent_id`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`bank_id`);
+
+--
+-- Indexes for table `bike`
+--
+ALTER TABLE `bike`
+  ADD PRIMARY KEY (`bike_id`);
+
+--
+-- Indexes for table `chart_of_account`
+--
+ALTER TABLE `chart_of_account`
+  ADD PRIMARY KEY (`coa_id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`cmp_id`);
+
+--
+-- Indexes for table `consignee`
+--
+ALTER TABLE `consignee`
+  ADD PRIMARY KEY (`consignee_id`);
+
+--
+-- Indexes for table `container`
+--
+ALTER TABLE `container`
+  ADD PRIMARY KEY (`container_id`);
+
+--
+-- Indexes for table `container_entry`
+--
+ALTER TABLE `container_entry`
+  ADD PRIMARY KEY (`ce_id`);
+
+--
+-- Indexes for table `container_movement`
+--
+ALTER TABLE `container_movement`
+  ADD PRIMARY KEY (`cm_id`);
+
+--
+-- Indexes for table `daily_description`
+--
+ALTER TABLE `daily_description`
+  ADD PRIMARY KEY (`dd_id`);
+
+--
+-- Indexes for table `diesel_entry`
+--
+ALTER TABLE `diesel_entry`
+  ADD PRIMARY KEY (`de_id`);
+
+--
+-- Indexes for table `diesel_limit`
+--
+ALTER TABLE `diesel_limit`
+  ADD PRIMARY KEY (`dl_id`);
+
+--
+-- Indexes for table `exin`
+--
+ALTER TABLE `exin`
+  ADD PRIMARY KEY (`exin_id`);
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`expense_id`);
+
+--
+-- Indexes for table `garage_entry`
+--
+ALTER TABLE `garage_entry`
+  ADD PRIMARY KEY (`ge_id`);
+
+--
+-- Indexes for table `income`
+--
+ALTER TABLE `income`
+  ADD PRIMARY KEY (`income_id`);
+
+--
+-- Indexes for table `line`
+--
+ALTER TABLE `line`
+  ADD PRIMARY KEY (`line_id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`login_id`);
+
+--
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`vehicle_id`);
+
+--
+-- Indexes for table `voucher`
+--
+ALTER TABLE `voucher`
+  ADD PRIMARY KEY (`voucher_number`);
+
+--
+-- Indexes for table `yard`
+--
+ALTER TABLE `yard`
+  ADD PRIMARY KEY (`yard_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts_entry`
+--
+ALTER TABLE `accounts_entry`
+  MODIFY `ae_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `agent`
+--
+ALTER TABLE `agent`
+  MODIFY `agent_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `bank_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `bike`
+--
+ALTER TABLE `bike`
+  MODIFY `bike_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `chart_of_account`
+--
+ALTER TABLE `chart_of_account`
+  MODIFY `coa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `cmp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `consignee`
+--
+ALTER TABLE `consignee`
+  MODIFY `consignee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `container`
+--
+ALTER TABLE `container`
+  MODIFY `container_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `container_entry`
+--
+ALTER TABLE `container_entry`
+  MODIFY `ce_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `container_movement`
+--
+ALTER TABLE `container_movement`
+  MODIFY `cm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `daily_description`
+--
+ALTER TABLE `daily_description`
+  MODIFY `dd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `diesel_entry`
+--
+ALTER TABLE `diesel_entry`
+  MODIFY `de_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `diesel_limit`
+--
+ALTER TABLE `diesel_limit`
+  MODIFY `dl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `exin`
+--
+ALTER TABLE `exin`
+  MODIFY `exin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `garage_entry`
+--
+ALTER TABLE `garage_entry`
+  MODIFY `ge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `income`
+--
+ALTER TABLE `income`
+  MODIFY `income_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `line`
+--
+ALTER TABLE `line`
+  MODIFY `line_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `voucher`
+--
+ALTER TABLE `voucher`
+  MODIFY `voucher_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `yard`
+--
+ALTER TABLE `yard`
+  MODIFY `yard_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
