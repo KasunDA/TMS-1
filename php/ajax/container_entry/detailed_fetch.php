5163ce82709c32c2e6da155b3d6cb499db4fe30b
UKKPK <36922935+UKKPK@users.noreply.github.com>
2018-05-12 21:51:45 +0500

    12

--- php/ajax/container_entry/detailed_fetch.php
+++ php/ajax/container_entry/detailed_fetch.php
@@ -7,7 +7,7 @@
 	$cm_id = $_GET['cm_id'];
 	
 
-	$sql = "SELECT a.vehicle_id ,b.container_size,b.index_number,c.driver_name,c.vehicle_number FROM container_entry a , container_movement b , vehicle c WHERE a.status=1 and a.vehicle_id= c.vehicle_id and a.cm_id=$cm_id";
+	$sql = "SELECT a.vehicle_id ,b.container_size,b.index_number FROM container_entry a , container_movement b WHERE a.status=1 and a.vehicle_id= c.vehicle_id and a.cm_id=$cm_id and b.cm_id=$cm_id";
 
 	$q = mysqli_query($mycon,$sql);
 	
@@ -17,8 +17,13 @@
 	{
 
 		$json[$n]['vehicle_id'] = $r['vehicle_id'];
-		$json[$n]['driver_name'] = $r['driver_name'];
-		$json[$n]['vehicle_number'] = $r['vehicle_number'];
+
+		$q1 = mysqli_query($mycon,"SELECT driver_name,vehicle_number from vehicle where vehicle_id=".$r['vehicle_id']);
+		$r1 = mysqli_fetch_array($q1);
+
+		$json[$n]['driver_name'] = $r1['driver_name'];
+		$json[$n]['vehicle_number'] = $r1['vehicle_number'];
+		
 		$json[$n]['container_size'] = $r['container_size'];
 		$json[$n]['index_number'] = $r['index_number'];
 
